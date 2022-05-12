e48e9c41088f36c232a86c789ed5d716f2ec5a2f
James Sayer <jsayer7@gmail.com>
2022-05-12 12:17:41 -0400

    Initial Commit

---
+++ SettingsStorage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SettingsStorage
+//
+//  Created by James Sayer on 5/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SettingsStorage/SettingsStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsStorageApp.swift
+//  SettingsStorage
+//
+//  Created by James Sayer on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SettingsStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
